4162bc3d2c0aa84b3818c144ababd85f6e9e5d7f
Arshaan Bhimani <ArshaanPC@Arshaans-MBP.lan>
2019-03-28 01:15:05 -0400

    Can scrape HTML and parse.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import * as $ from 'jquery';
+import * as pup from 'puppeteer';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  myObservable;
+  searchTerms;
+  
+  constructor(public httpClient: HttpClient) {
+  }
+  
   ngOnInit() {
-    console.log("Hello.");
+    this.searchTerms = [];
+    // this.myObservable = this.httpClient.get(
+    //   'https://cors-anywhere.herokuapp.com/https://trends.google.com/trends/trendingsearches/daily?geo=US',
+    //   { responseType: 'text' }
+    //   );
+    // this.myObservable.subscribe( 
+    //   (res) => { 
+    //     // console.log($(res).find('ng-include > div > div').length);
+    //     console.log(res);
+    //   })
+    
   }
 }
