d05570205564c24fb8d158c2c9e93458efdba5d2
Arshaan Bhimani <ArshaanPC@Arshaans-MBP.lan>
2019-03-27 23:57:40 -0400

    Updated README.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'google-search-engine-battle';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    console.log("Hello.");
+  }
 }
