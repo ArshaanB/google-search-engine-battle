8ff4f94173117f2fa85bb5c9ebe1506c086e3851
Arshaan Bhimani <aa2bhima@uwaterloo.ca>
2019-04-05 23:08:41 -0400

    most game functionality is created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,12 @@ export class AppComponent implements OnInit {
   myObservable;
   searchTerms;
   searchTermsVolume;
+  term1: {name: string, volume: number};
+  term2: {name: string, volume: number};
+  winningTerm = "";
+  playButtonFlag = true;
+  score = 0;
+  lives = 3;
   
   constructor(public httpClient: HttpClient) {
   }
@@ -28,7 +34,7 @@ export class AppComponent implements OnInit {
         $(res).find('tbody > tr')
         .each(
           (index, element) => {
-            if (index < 10) {
+            if (index < 50) {
               this.searchTerms.push($(element).find('td:nth-child(2)').text());
               this.searchTermsVolume.push($(element).find('td:nth-child(3)').text());
             }
@@ -37,9 +43,37 @@ export class AppComponent implements OnInit {
       });
   }
 
-  myURL(term: String) {
+  myURL(term: number) {
+    let myTerm = this.term1;
+    if (term === 2) myTerm = this.term2;
+    return ("https://source.unsplash.com/1600x900/?" + myTerm.name);
     // return ("https://source.unsplash.com/featured/?" + term);
-    return ("https://source.unsplash.com/1600x900/?" + term);
     // https://www.flickr.com/services/api/flickr.photos.search.html
+  }
+
+  populateTerms() {
+    let termIndex1 = Math.floor(Math.random() * 50);
+    let termIndex2 = Math.floor(Math.random() * 50);
+    this.term1 = {name: this.searchTerms[termIndex1], volume: this.searchTermsVolume[termIndex1]};
+    this.term2 = {name: this.searchTerms[termIndex2], volume: this.searchTermsVolume[termIndex2]};
+    this.playButtonFlag = false;
+    if (this.term1.volume > this.term2.volume) {
+      this.winningTerm = this.term1.name;
+    } else {
+      this.winningTerm = this.term2.name;
+    }
+  }
+
+  onAnswer(answer: number) {
+    if ((answer == 1) && (this.winningTerm == this.term1.name)) {
+      this.score++;
+    }
+    else if ((answer == 2) && (this.winningTerm == this.term2.name)) {
+      this.score++;
+    }
+    else {
+      this.lives--;
+    }
+    this.populateTerms();
   }
 }
